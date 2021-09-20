466b271f18bd22939c72e835588d9c2a64dc354b
Mateusz Budzynski <mati.leeder@gmail.com>
2021-09-20 12:26:09 +0200

    first commit with autodeploys

---
+++ app.ts
@@ -0,0 +1,23 @@
+import { Client, Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+})
+
+const { prefix } = require('./config.json')
+
+client.on('ready', () => {
+  console.log('Ready')
+})
+
+client.on('messageCreate', (message) => {
+  if (message.content === 'ping') {
+    message.reply({
+      content: 'pong!!',
+    })
+  }
+})
+
+client.login(process.env.DJSTOKEN)
